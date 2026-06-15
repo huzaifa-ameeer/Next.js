180d44ffde88922dfc5e858bfdcd63dc97a4ef91
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-06-15 12:36:25 +0500

    learned about layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <h1>This is a layout page</h1>
+        {children}
+        </body>
     </html>
   );
 }
